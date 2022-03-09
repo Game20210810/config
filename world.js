1feabf3594346d4a07e3e8379f279b0234dd4d99
Game20210810 <88695500+Game20210810@users.noreply.github.com>
2022-03-09 16:59:05 +0800

    Update world.js

--- world.js
+++ world.js
@@ -1,7 +1,6 @@
 [
-    "tGhWEk08Y1SVHlZJy8b6cXvxjOzbwmKfV6dB+oQpCmE=",
-    "tGhWEk08Y1SVHlZJy8b6cS0pN4M6z650830xqtPpq11/MTZ6yRwcpVaabCqnMXum",
-    "tGhWEk08Y1SVHlZJy8b6cUw/i1X5fdRdKq6Zois+2v4=",
-    "UPHCj8Ci45pYi5jcJML8kDLWGXIpJI/cGgdNhi7TvyN/MTZ6yRwcpVaabCqnMXum",
-    "UPHCj8Ci45pYi5jcJML8kLL0/xVwA4zvHuZ2y8F5tc1/MTZ6yRwcpVaabCqnMXum"
+    "UPHCj8Ci45pYi5jcJML8kOGEYZXbtrLECmzDj/c2yCd/MTZ6yRwcpVaabCqnMXum",
+    "UPHCj8Ci45pYi5jcJML8kPj1Cg3U/eZAxrHrM07Ovfi/uk5VwTDrROQT0epeVz6s",
+    "UPHCj8Ci45pYi5jcJML8kEs17mdQkYTPStCEOno9bDw=",
+    "UPHCj8Ci45pYi5jcJML8kDLWGXIpJI/cGgdNhi7TvyN/MTZ6yRwcpVaabCqnMXum"
 ]
