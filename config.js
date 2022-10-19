49904c0cd81826d8c59f5e57513f4923a4fd3b43
Game20210810 <88695500+Game20210810@users.noreply.github.com>
2022-10-19 10:18:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,2 @@
 [
-  "tGhWEk08Y1SVHlZJy8b6cXAorbrVM8kGIkKju6zxhxk=",
-  "rCAwvWoIlLv1L4PdNekhdHTgJdJF46VBvywjlmD1SSU=",
-  "tGhWEk08Y1SVHlZJy8b6cflfjmr2hkIql9GJcb5Z+OE=",
-  "tGhWEk08Y1SVHlZJy8b6cYoUfT7woxiCTp/8LIN05QM=",
-  "tGhWEk08Y1SVHlZJy8b6cZ37RTdStzhccUTrgTzTY0M="
 ]
