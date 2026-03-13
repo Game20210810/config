5b6726b94a990c943faf71b632dc5ac957c18683
Game20210810 <88695500+Game20210810@users.noreply.github.com>
2026-03-13 16:15:56 +0800

    Update world.js
    clear data

--- world.js
+++ world.js
@@ -1,6 +1,3 @@
 [
-    "UPHCj8Ci45pYi5jcJML8kOGEYZXbtrLECmzDj/c2yCd/MTZ6yRwcpVaabCqnMXum",
-    "UPHCj8Ci45pYi5jcJML8kPj1Cg3U/eZAxrHrM07Ovfi/uk5VwTDrROQT0epeVz6s",
-    "UPHCj8Ci45pYi5jcJML8kEs17mdQkYTPStCEOno9bDw=",
-    "UPHCj8Ci45pYi5jcJML8kDLWGXIpJI/cGgdNhi7TvyN/MTZ6yRwcpVaabCqnMXum"
+   
 ]
